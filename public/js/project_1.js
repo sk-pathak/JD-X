c34c2a7134cf969a93b365530ddf25f866fff57c
Sumit <sumitpathak2002@gmail.com>
2023-07-12 23:29:26 +0530

    alumni talks done

--- public/js/project_1.js
+++ public/js/project_1.js
@@ -1,3 +1,3 @@
+document.getElementsByClassName("temp")[0].style.scrollSnapAlign="none";
 var footer = document.querySelector("footer");
 footer.style.scrollSnapAlign = "none";
-document.getElementsByClassName("temp")[0].style.scrollSnapAlign="none";
