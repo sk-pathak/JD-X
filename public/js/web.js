c8fa761a4eb765e43a19443e5ee3bd4f7492bc66
Sumit <sumitpathak2002@gmail.com>
2023-07-14 20:11:59 +0530

    made community responsive, included loader

---
+++ public/js/web.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function () {
+    var content = document.getElementById('init');
+    let element = document.getElementById('animater');
+    let loader = document.getElementById('loader');
+
+    var words = [" PUSH", " PULL"];
+    var index = 0;
+
+    function alternateWords() {
+        content.innerHTML = words[index];
+        index = (index + 1) % words.length;
+
+        setTimeout(alternateWords, 700); // Change the delay (in milliseconds) between word changes if needed
+    }
+    
+    setTimeout(function () {
+        content.classList.add('slide-down');
+        alternateWords();
+        element.classList.remove('typewriter');
+    }, 1000);
+    setTimeout(function () {
+        loader.remove();
+        let main = document.getElementById('main');
+        main.style.display = 'block';
+    }, 3000);
+});
